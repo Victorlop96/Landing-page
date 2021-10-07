edc095853af04b80bb30eeb806b72b5959b04916
Victorlop96 <vlop1096@gmail.com>
2021-10-07 02:37:01 -0300

    add cartas no corre

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,7 @@ import "../styles/index.scss";
 import Home from "./component/home.jsx";
 import Main from "./main.jsx";
 import Navbar from "./component/Navbar.jsx";
+import Card from "./component/Card";
 import Footer from "./component/Footer.jsx";
 
 //render your react application
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -7,6 +7,7 @@ const Main = () => {
 	return (
 		<>
 			<Navbar />
+			<Card />
 			<Footer />
 		</>
 	);
